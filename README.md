179047eee12387863bda20f600fb480e5c0e4d9a
kjansen <kris.jansen@perceptivesoftware.com>
2014-12-27 15:55:07 +0100

    First try's to get the tomcat server running

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Store and retrieve the SmartMeter data.
 
 Got a lot of my inspiration from this website:
 http://crunchify.com/create-very-simple-jersey-rest-service-and-send-json-data-from-java-client/
+and here:
+http://www.mkyong.com/webservices/jax-rs/jersey-hello-world-example/
 
 
--- src/main/java/com/soldev/DataManager.java
+++ src/main/java/com/soldev/DataManager.java
@@ -1,14 +1,11 @@
 package com.soldev;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 
 /**
@@ -17,14 +14,16 @@ import javax.ws.rs.core.Response;
  */
 @Path("/")
 public class DataManager {
+
     @POST
-    @Path("/dataManager")
+    @Path("/DataManagerService")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response dataManagerREST(InputStream incomingData) {
         StringBuilder dataManagerBuilder = new StringBuilder();
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
-            String line = null;
+            //String line = null;
+            String line;
             while ((line = in.readLine()) != null) {
                 dataManagerBuilder.append(line);
             }
@@ -35,5 +34,15 @@ public class DataManager {
 
         // return HTTP response 200 in case of success
         return Response.status(200).entity(dataManagerBuilder.toString()).build();
+    }
+
+    @GET
+    @Path("/amIAlive/{param}")
+    public Response amIAlive(@PathParam("param") String msg) {
+
+        String output = "DataManger Is ALIVE : " + msg;
+
+        return Response.status(200).entity(output).build();
+
     }
 }
